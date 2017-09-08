2e13574c785377a19de892c09ca8ecab9e6f26ff
岸本 諒太 <ryotakishimoto@kishimoto-no-macbook-air.local>
2017-09-08 22:22:49 +0900

    作成

---
+++ SlideshowApp/BigViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BigViewController.swift
+//  SlideshowApp
+//
+//  Created by 岸本 諒太 on 2017/09/07.
+//  Copyright © 2017年 ryota.kishimoto. All rights reserved.
+//
+
+import UIKit
+
+class BigViewController: UIViewController {
+
+    @IBOutlet weak var `return`: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let photos = ["画像1.jpeg","画像2.jpeg","画像3.jpeg"]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet var TapImage_out: UITapGestureRecognizer!
+    @IBAction func TapImage(_ sender:UITapGestureRecognizer) {
+        performSegue(withIdentifier:"big",sender:nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let image:UIImage! = UIImage(named:photos[0])
+        imageView.image = image
+    
     }
-
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()  {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 
 }
 
